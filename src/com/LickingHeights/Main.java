29d17a1f2dacc65f4387e92eb9f423d957bc3b4e
Teagan Lee <teagan.lee.2020@student.lhschools.org>
2020-02-26 13:53:01 -0500

    Oddness Method Created

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,22 @@
 package com.LickingHeights;
 
+        import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner keyboard;
+        keyboard = new Scanner (System.in);
+
+        int number;
+
+        System.out.println("Insert a POSITIVE #!!");
+        number = keyboard.nextInt();
+
+        System.out.println("number is Even: " + (number % 2 ==0));
+        System.out.println("number is Odd: " + (number % 2 !=0));
+
     }
 }
