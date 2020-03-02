6a73a581d394b98fefbf8d3be0a2bbaabaca1ae2
Teagan Lee <teagan.lee.2020@student.lhschools.org>
2020-03-02 13:17:24 -0500

    keepContainedMethodCreated

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,17 +6,42 @@ public class Main {
 
 
     public static void main(String[] args) {
+        testOddness();
+        keepContained();
+    }
 
+        public static void testOddness() {
+
+            //Odd And Even
+
+
+            Scanner keyboard;
+            keyboard = new Scanner(System.in);
+
+            int number;
+
+            System.out.println("Insert a POSITIVE #!!");
+            number = keyboard.nextInt();
+
+            System.out.println("number is Even: " + (number % 2 == 0));
+            System.out.println("number is Odd: " + (number % 2 != 0));
+            System.out.println("");
+
+        }
+    public static void keepContained(){
+        // Keep It Contained
         Scanner keyboard;
-        keyboard = new Scanner (System.in);
+        keyboard = new Scanner(System.in);
 
         int number;
 
         System.out.println("Insert a POSITIVE #!!");
         number = keyboard.nextInt();
 
-        System.out.println("number is Even: " + (number % 2 ==0));
-        System.out.println("number is Odd: " + (number % 2 !=0));
+        System.out.print("Number contained is: " + ((number%8)+5 ));
+
+
+
 
     }
 }
